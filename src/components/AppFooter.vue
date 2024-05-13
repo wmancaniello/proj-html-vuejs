e7d1ead9f539de58ad2425d4dcd4561a9d707add
wmancaniello <wmancaniello@gmail.com>
2024-05-13 17:33:57 +0200

    Fix

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,36 +1,37 @@
 <script>
 export default {
-    data() {
-        return {
-            menu: [
-                {
-                    title: "Home",
-                    routeName: "home",
-                },
-                {
-                    title: "About",
-                    routeName: "about",
-                },
-                {
-                    title: "Contacts",
-                    routeName: "contacts",
-                }
-            ]
-        }
-    }
-}
+  data() {
+    return {
+      menu: [
+        {
+          title: "Home",
+          routeName: "home",
+        },
+        {
+          title: "About",
+          routeName: "about",
+        },
+        {
+          title: "Contacts",
+          routeName: "contacts",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <template>
-    <footer>
-        <ul>
-            <li v-for="item in menu"> 
-                <router-link :to="{name: item.routeName}" >
-                    {{ item.title }}
-                </router-link>
-             </li>
-        </ul>
-    </footer>
+  <footer>
+    <h1>Footer</h1>
+    <ul>
+      <li v-for="item in menu" :key="item">
+        <router-link :to="{ name: item.routeName }">
+          {{ item.title }}
+        </router-link>
+      </li>
+    </ul>
+  </footer>
 </template>
 
 <style lang="scss" scoped></style>
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,36 +1,37 @@
 <script>
 export default {
-    data() {
-        return {
-            menu: [
-                {
-                    title: "Home",
-                    routeName: "home",
-                },
-                {
-                    title: "About",
-                    routeName: "about",
-                },
-                {
-                    title: "Contacts",
-                    routeName: "contacts",
-                }
-            ]
-        }
-    }
-}
+  data() {
+    return {
+      menu: [
+        {
+          title: "Home",
+          routeName: "home",
+        },
+        {
+          title: "About",
+          routeName: "about",
+        },
+        {
+          title: "Contacts",
+          routeName: "contacts",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <template>
-    <header>
-        <ul>
-            <li v-for="item in menu"> 
-                <router-link :to="{name: item.routeName}" >
-                    {{ item.title }}
-                </router-link>
-             </li>
-        </ul>
-    </header>
+  <header>
+    <h1>Header</h1>
+    <ul>
+      <li v-for="item in menu" :key="item">
+        <router-link :to="{ name: item.routeName }">
+          {{ item.title }}
+        </router-link>
+      </li>
+    </ul>
+  </header>
 </template>
 
 <style lang="scss" scoped></style>
