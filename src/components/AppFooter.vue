0d8e82892ccb50abc8aaeab49008db3992c6713a
francesco-mauro <francesco.mauro180@gmail.com>
2024-05-13 17:28:04 +0200

    feat: footer and scaffolding

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,9 +1,36 @@
 <script>
-export default {};
+export default {
+    data() {
+        return {
+            menu: [
+                {
+                    title: "Home",
+                    routeName: "home",
+                },
+                {
+                    title: "About",
+                    routeName: "about",
+                },
+                {
+                    title: "Contacts",
+                    routeName: "contacts",
+                }
+            ]
+        }
+    }
+}
 </script>
 
 <template>
-    <h1>Footer</h1>
+    <footer>
+        <ul>
+            <li v-for="item in menu"> 
+                <router-link :to="{name: item.routeName}" >
+                    {{ item.title }}
+                </router-link>
+             </li>
+        </ul>
+    </footer>
 </template>
 
 <style lang="scss" scoped></style>
