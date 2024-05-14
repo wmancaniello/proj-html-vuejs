4bba32e9c236b548e28a843c410d220e912b069b
francesco-mauro <francesco.mauro180@gmail.com>
2024-05-14 11:51:47 +0200

    add: hover effects and various cleaning

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -36,45 +36,73 @@ export default {
 
 <template>
     <div class="container">
-  <header class="d-flex flex-wrap justify-content-between align-items-center py-3 mb-4">
-    <!-- LINKS DOWNLOAD APP, ADVERTISE -->
-    <div class="d-flex align-items-center">
-      <a href="https://1.envato.market/qjLX5" class="nav-link">Download App <span class="badge bg-warning text-dark">iOS</span></a>
-      <a href="https://1.envato.market/qjLX5" class="nav-link ms-3">Advertise with us</a>
-    </div>
-    
-    <!-- Logo -->
-    <a href="/" class="d-flex align-items-center mx-auto mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-      <img src="https://avada.website/food/wp-content/uploads/sites/118/2021/04/avada-food-logo.svg" alt="Avada Food" width="40" height="32" class="me-2">
-    </a>
-    
-    <!-- Social media links -->
-    <div class="d-flex align-items-center">
-        <span class="fs-4">Simple header</span>
-      <a href="https://www.facebook.com/ThemeFusion-101565403356430/" class="nav-link"><i class="bi bi-facebook"></i></a>
-      <a href="https://www.instagram.com/themefusion/" class="nav-link"><i class="bi bi-instagram"></i></a>
-      <a href="https://twitter.com/theme_fusion" class="nav-link"><i class="bi bi-twitter"></i></a>
-      <a href="https://www.youtube.com/@AvadaVideos" class="nav-link"><i class="bi bi-youtube"></i></a>
-    </div>
-  </header>
-</div>
+      <header class="d-flex flex-wrap justify-content-between align-items-start py-3 mb-4">
+        <!-- ------------------LINKS DOWNLOAD APP, ADVERTISE------------------ -->
+        <div class="d-flex align-items-start w-30">
+          <a href="https://1.envato.market/qjLX5" class="nav-link orange-hover">Download App <span class="badge bg-warning text-dark">iOS</span></a>
+          <a href="https://1.envato.market/qjLX5" class="nav-link ms-3 orange-hover">Advertise with us</a>
+        </div>
+        <!-- ------------------LINKS DOWNLOAD APP, ADVERTISE------------------ -->
 
-<div class="px-4 py-5 my-5 text-center">
-  <img class="d-block mx-auto mb-4" src="https://avada.website/food/wp-content/uploads/sites/118/2021/04/avada-food-logo.svg" alt="" width="152" height="121">
-  <h1 class="display-5 fw-bold text-body-emphasis">Centered hero</h1>
-  <div class="col-lg-6 mx-auto">
-    <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
-    <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-      <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
-      <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button>
-    </div>
-  </div>
-</div>
+        
+        <!-- ------------------Logo------------------ -->
+        <div class="d-flex align-items-center">
+          <a href="/" class="d-flex align-items-center mx-auto mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+            <img src="https://avada.website/food/wp-content/uploads/sites/118/2021/04/avada-food-logo.svg" alt="Avada Food" width="40" height="32" class="logo">
+          </a>
+        </div>
+        <!-- ------------------Logo------------------ -->
 
-</template>
+        
+        <!-- ------------------Social media links------------------ -->
+        <div class="d-flex align-items-center w-30">
+          <span class="fs-4">Simple header</span>
+          <a href="https://www.facebook.com/ThemeFusion-101565403356430/" class="nav-link"><i class="bi bi-facebook"></i></a>
+          <a href="https://www.instagram.com/themefusion/" class="nav-link"><i class="bi bi-instagram"></i></a>
+          <a href="https://twitter.com/theme_fusion" class="nav-link"><i class="bi bi-twitter"></i></a>
+          <a href="https://www.youtube.com/@AvadaVideos" class="nav-link"><i class="bi bi-youtube"></i></a>
+        </div>
+        <!-- ------------------Social media links------------------ -->
+
+      </header>
+
+    </div>
+  
+
+    <!-- STAGING BOTTOM -->
+    <div class="px-4 py-5 my-5 text-center">
+      <img class="d-block mx-auto mb-4" src="https://avada.website/food/wp-content/uploads/sites/118/2021/04/avada-food-logo.svg" alt="" width="152" height="121">
+      <h1 class="display-5 fw-bold text-body-emphasis">Centered hero</h1>
+      <div class="col-lg-6 mx-auto">
+        <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p>
+        <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+          <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
+          <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button>
+        </div>
+      </div>
+    </div>
+    <!-- STAGING BOTTOM -->
+
+  </template>
+  
   
 
 <style scoped>
+
+/* ------------------SIZES------------------------- */
+
+.w-30 {
+  width: 30%;
+}
+
+/* ------------------SIZES------------------------- */
+
+
+.logo {
+  width: 200%; 
+  height: auto;
+}
+
 .nav-link {
   color: #000;
   margin-right: 10px;
@@ -96,6 +124,12 @@ export default {
 
 .nav-link:hover .bi {
   color: #ff6600;
+}
+
+/* ------------------HOVER EFFECTS-------------------------------- */
+
+.orange-hover:hover {
+  color: #ff6600; /* Colore arancione */
 }
 </style>
   
