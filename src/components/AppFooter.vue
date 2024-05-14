37aa7faec230cb7ee0a5c077dfae698795c994ff
wmancaniello <wmancaniello@gmail.com>
2024-05-14 11:18:54 +0200

    add: create section footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -30,7 +30,48 @@ export default {
 </script>
 
 <template>
-  <footer></footer>
+  <div class="container-fluid text-white p-3 ms_footer">
+    <!-- Spazio immagini -->
+    <div class="mt-3">
+      <h3 class="text-center fw-bold">Spazio per immagini</h3>
+    </div>
+    <hr>
+    <!-- Spazio immagini -->
+
+    <div class="row justify-content-around text-center text-md-start">
+      <div class="col-md-2 text-center">
+        <h3>Logo img</h3>
+        <p>icone social</p>
+      </div>
+
+      <!-- Section 1 -->
+      <div class="col-md-2">
+        <ul class="list-unstyled">
+          <li v-for="item in menu" :key="item" class="my-3">
+            <router-link :to="{ name: item.routeName }">
+              {{ item.title }}
+            </router-link>
+          </li>
+        </ul>
+      </div>
+      <!-- /Section 1 -->
+
+      <!-- Section 2 -->
+      <div class="col-md-2">
+        <h3>
+          Spazio per contact US
+        </h3>
+      </div>
+      <!-- /Section 2 -->
+
+      <!-- Section 3  -->
+      <div class="col-md-2">
+        <h3>Spazio per Get our App</h3>
+      </div>
+      <!-- /Section 3 -->
+
+    </div>
+  </div>
 
   <!-- <footer>
     <h1>Footer</h1>
@@ -44,4 +85,8 @@ export default {
   </footer> -->
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.ms_footer {
+  background-color: black;
+}
+</style>
