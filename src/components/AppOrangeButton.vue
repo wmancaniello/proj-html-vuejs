13d11992d7de3bdd161dbc763b7a123c82b5113f
SimoneMicheleAllocca <s.allocca1998@gmail.com>
2024-05-15 15:51:38 +0200

    add: orange-button

---
+++ src/components/AppOrangeButton.vue
@@ -0,0 +1,16 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <a href="" class="btn btn-primary ms-btn"></a>
+</template>
+
+<style scoped>
+.ms-btn {
+  background-color: #fc7523;
+  color: white;
+  border: none;
+  border-radius: 0%;
+}
+</style>
