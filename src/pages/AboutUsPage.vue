de8abdddae6673f4d9f9c79ae0401a00e8dce423
francesco-mauro <francesco.mauro180@gmail.com>
2024-05-15 11:38:18 +0200

    add: about page fixes

--- src/pages/AboutUsPage.vue
+++ src/pages/AboutUsPage.vue
@@ -6,153 +6,216 @@
 
 <template>
     <div>
-      <!-- Container con background image e overlay scuro -->
-      <div class="hero-section">
-        <div class="overlay">
-          <div class="text-center">
-            <h1>About Us</h1>
-            <p>About Avada Food Blog. About our amazing journey so far</p>
-          </div>
+        <!-- Container con background image e overlay scuro -->
+        <div class="hero-section">
+            <div class="overlay">
+                <div class="text-center">
+                    <h1>About Us</h1>
+                    <p>About Avada Food Blog. About our amazing journey so far</p>
+                </div>
+            </div>
         </div>
-      </div>
-  
-      <!-- sezione con griglia di immagini e testo -->
-      <div class="container my-5">
-        <div class="row">
-          <!-- Colonna di sinistra con la griglia di immagini -->
-          <div class="col-md-6">
+
+        <!-- sezione con griglia di immagini e testo -->
+        <div class="container my-5">
             <div class="row">
-              <div class="col-6 p-1">
-                <img src="../assets/img/banana.jpg" alt="Image 1" class="img-fluid">
-              </div>
-              <div class="col-6 p-1">
-                <img src="../assets/img/beans.jpg" alt="Image 2" class="img-fluid">
-              </div>
-              <div class="col-6 p-1">
-                <img src="../assets/img/kimchi.jpg" alt="Image 3" class="img-fluid">
-              </div>
-              <div class="col-6 p-1">
-                <img src="../assets/img/egg-fruit.jpg" alt="Image 4" class="img-fluid">
-              </div>
-              <div class="col-6 p-1">
-                <img src="../assets/img/ice-cream.jpg" alt="Image 5" class="img-fluid">
-              </div>
-              <div class="col-6 p-1">
-                <img src="../assets/img/sushi.jpg" alt="Image 6" class="img-fluid">
-              </div>
+                <!-- Colonna di sinistra con la griglia di immagini -->
+                <div class="col-md-6">
+                    <div class="row">
+                        <div class="col-6 p-1">
+                            <img src="../assets/img/banana.jpg" alt="Image 1" class="img-fluid">
+                        </div>
+                        <div class="col-6 p-1">
+                            <img src="../assets/img/beans.jpg" alt="Image 2" class="img-fluid">
+                        </div>
+                        <div class="col-6 p-1">
+                            <img src="../assets/img/kimchi.jpg" alt="Image 3" class="img-fluid">
+                        </div>
+                        <div class="col-6 p-1">
+                            <img src="../assets/img/egg-fruit.jpg" alt="Image 4" class="img-fluid">
+                        </div>
+                        <div class="col-6 p-1">
+                            <img src="../assets/img/ice-cream.jpg" alt="Image 5" class="img-fluid">
+                        </div>
+                        <div class="col-6 p-1">
+                            <img src="../assets/img/sushi.jpg" alt="Image 6" class="img-fluid">
+                        </div>
+                        <div class="col-6 p-1">
+                            <img src="../assets/img/banana.jpg" alt="Image 1" class="img-fluid">
+                        </div>
+                        <div class="col-6 p-1">
+                            <img src="../assets/img/beans.jpg" alt="Image 2" class="img-fluid">
+                        </div>
+                    </div>
+                </div>
+                <!-- Colonna di destra con il testo -->
+                <div class="col-md-6">
+                    <div class="row mb-5">
+                        <div class="col-12">
+                            <h3>Our Story</h3>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent in varius ante. Sed
+                                quis luctus eros. Phasellus rutrum laoreet leo in consequat. Phasellus vel semper ante.
+                                Vivamus turpis purus, dapibus vitae ex a, varius ornare elit. Nullam eu purus congue,
+                                bibendum felis id, aliquet ante.</p>
+                            <p>Donec porttitor, diam quis posuere lacinia, justo libero consequat mi, at euismod lacus
+                                mi in erat. Cras sit amet ornare orci.</p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-12">
+                            <h3>About Our Company</h3>
+                            <p>Curabitur id nibh sit amet lectus efficitur tempus aliquet non ante. Aenean feugiat, enim
+                                suscipit dictum ullamcorper, tellus nunc mollis leo, vitae elementum lacus lorem tempor
+                                ante. Maecenas ac aliquet risus, nec fringilla sem. Nam sed nibh id arcu vestibulum
+                                malesuada pretium sed nisl. Nunc tristique, dui ac interdum scelerisque, arcu purus
+                                faucibus nisl.</p>
+                            <p>Praesent condimentum ligula lacus, id dignissim mi feugiat non. Nulla vel velit nisi. Nam
+                                vitae eros ullamcorper dolor dapibus pellentesque. In non iaculis.</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- Sezione con icone e testo -->
+                <div class="container my-5">
+                    <div class="row text-center">
+                        <div class="col-md-4">
+                            <div class="icon-text">
+                                <div>
+                                    <i class="fa-solid fa-square-check custom-icon"></i>
+                                </div>
+                                <h4>AUTHENTICITY</h4>
+                                <p>Aenean eu arcu sed velit mattis vehicula ac quis tellus. Suspendisse elementum justo
+                                    neque, a tincidunt est congue quis.</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="icon-text">
+                                <i class="fa-solid fa-file-lines custom-icon"></i>
+                                <h4>REAL REVIEWS</h4>
+                                <p>Praesent erat eros, malesuada nec augue non, aliquet finibus orci. Duis vitae cursus
+                                    tortor. Suspendisse blandit volutpat.</p>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="icon-text">
+                                <i class="fa-solid fa-utensils custom-icon"></i>
+                                <h4>UNIQUE RECIPES</h4>
+                                <p>Donec vel volutpat erat. Sed in nulla vitae elit accumsan tempus. Aliquam dapibus
+                                    ligula gravida, blandit enim ac, sagittis justo.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-          <!-- Colonna di destra con il testo -->
-          <div class="col-md-6">
-            <div class="row mb-5">
-              <div class="col-12">
-                <h3>Our Story</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent in varius ante. Sed quis luctus eros. Phasellus rutrum laoreet leo in consequat. Phasellus vel semper ante. Vivamus turpis purus, dapibus vitae ex a, varius ornare elit. Nullam eu purus congue, bibendum felis id, aliquet ante.</p>
-                <p>Donec porttitor, diam quis posuere lacinia, justo libero consequat mi, at euismod lacus mi in erat. Cras sit amet ornare orci.</p>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col-12">
-                <h3>About Our Company</h3>
-                <p>Curabitur id nibh sit amet lectus efficitur tempus aliquet non ante. Aenean feugiat, enim suscipit dictum ullamcorper, tellus nunc mollis leo, vitae elementum lacus lorem tempor ante. Maecenas ac aliquet risus, nec fringilla sem. Nam sed nibh id arcu vestibulum malesuada pretium sed nisl. Nunc tristique, dui ac interdum scelerisque, arcu purus faucibus nisl.</p>
-                <p>Praesent condimentum ligula lacus, id dignissim mi feugiat non. Nulla vel velit nisi. Nam vitae eros ullamcorper dolor dapibus pellentesque. In non iaculis.</p>
-              </div>
-            </div>
-          </div>
         </div>
-      </div>
     </div>
-  </template>
-    
-  <style scoped lang="scss">
+</template>
+
+<style scoped lang="scss">
+    /* ----------------------- HERO SECTION -------------------------------- */
+
     .hero-section {
-      position: relative;
-      background-image: url("../assets/img/Avada-AboutUs-Background.png"); 
-      background-size: cover;
-      background-position: center;
-      background-repeat: no-repeat;
-      height: 400px;
+        position: relative;
+        background-image: url("../assets/img/Avada-AboutUs-Background.png");
+        background-color: #67615A;
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        height: 400px;
     }
-    
+
     .overlay {
-      position: absolute;
-      top: 0;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      background-color: rgba(0, 0, 0, 0.5);
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      text-align: center;
-      color: white;
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+        color: white;
     }
-    
+
     .overlay h1 {
-      font-family: 'Playfair Display', serif;
-      font-size: 3rem;
-      margin-bottom: 10px;
+        font-family: 'Playfair Display', serif;
+        font-size: 3rem;
+        margin-bottom: 10px;
     }
-    
+
     .overlay p {
-      font-family: 'Roboto', sans-serif;
-      font-size: 1.2rem;
+        font-family: 'Roboto', sans-serif;
+        font-size: 1.2rem;
+        color: azure;
     }
-    
+
+    /* ------------------------------Font AWSOME ----------------------------------------------- */
+
+    .icon-text {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    .custom-icon {
+        font-size: 3em;
+        margin-bottom: 10px;
+    }
+
+
+
     h3 {
-      font-family: 'Playfair Display', serif;
-      font-weight: bold;
-      margin-bottom: 20px;
+        font-family: 'Playfair Display', serif;
+        font-weight: bold;
+        margin-bottom: 20px;
     }
-    
+
     p {
-      font-family: 'Roboto', sans-serif;
-      font-size: 16px;
-      line-height: 1.5;
-      color: #666;
-      margin-bottom: 20px;
+        font-family: 'Roboto', sans-serif;
+        font-size: 16px;
+        line-height: 1.5;
+        color: #666;
+        margin-bottom: 20px;
     }
-    
+
     .ms-container-card {
-      margin: auto;
-      background-color: #ffffff;
-      margin-top: -100px;
+        margin: auto;
+        background-color: #ffffff;
+        margin-top: -100px;
     }
-    
+
     .ms-btn,
     .ms-btn-home {
-      background-color: #fc7523;
-      border: none;
+        background-color: #fc7523;
+        border: none;
     }
-    
+
     .ms-img {
-      height: 200px;
-      object-fit: cover;
+        height: 200px;
+        object-fit: cover;
     }
-    
+
     a {
-      text-decoration: none;
-      color: black;
+        text-decoration: none;
+        color: black;
     }
-    
+
     .ms-banner-text:hover {
-      color: #fc7523;
+        color: #fc7523;
     }
-    
+
     .card-text {
-      color: #666;
+        color: #666;
     }
-    
+
     h2 {
-      font-family: 'Playfair Display', serif;
-      color: #333;
+        font-family: 'Playfair Display', serif;
+        color: #333;
     }
-    
+
     p.card-text {
-      font-family: 'Roboto', sans-serif;
-      font-size: 16px;
-      line-height: 1.5;
-      color: #666;
+        font-family: 'Roboto', sans-serif;
+        font-size: 16px;
+        line-height: 1.5;
+        color: #666;
     }
-    </style>
-        
+</style>
